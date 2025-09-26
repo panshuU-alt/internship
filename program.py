6ef451350b4e67e75244342435a74bce1f4eaba4
Ashikhmina Liza <lizunchi@bk.ru>
2025-09-26 19:19:16 +0300

    Added addition operation: sum of two numbers

---
+++ program.py
@@ -0,0 +1,7 @@
+print("Number operations program")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Mathematical operation - addition
+result = num1 + num2
+print(f"The sum of {num1} and {num2} is: {result}")
