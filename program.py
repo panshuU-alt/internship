0361f732ca615fe3c3c47dafd63788a4bbffd639
Ashikhmina Liza <lizunchi@bk.ru>
2025-09-26 19:45:56 +0300

    Changed addition to subtraction via GitHub

--- program.py
+++ program.py
@@ -3,5 +3,5 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # Mathematical operation - addition
-result = num1 + num2
-print(f"The sum of {num1} and {num2} is: {result}")
+result = num1 - num2
+print(f"The difference of {num1} and {num2} is: {result}")
